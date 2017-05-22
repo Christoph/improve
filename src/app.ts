c73351c7a6955358a7c7d1fa6e6363864f679cb0
Chris <christoph.kralj@gmail.com>
2017-05-22 15:55:34 +0200

    Added new files for vpsa stuff

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,8 @@ export class App {
       config.title = "Contacts";
       config.map([
           { route: "", moduleId: "empty/no-selection", nav: true, title: "Select" },
-          { route: "vis/", moduleId: "vis/vis", nav: true, title: "Vis", name: "vis" }
+          { route: "vis/", moduleId: "vis/vis", nav: true, title: "Vis", name: "vis" },
+          { route: "vpsa/", moduleId: "vpsa/vpsa", nav: true, title: "VPSA", name: "vpsa" }
       ]);
 
       this.router = router;
---
+++ src/vpsa/vpsa.ts
@@ -0,0 +1,12 @@
+export class Vspa {
+    data = [
+        {"salesperson" : "Bob", "sales": 12},
+        {"salesperson" : "Robin", "sales": 1},
+        {"salesperson" : "Anne", "sales": 8},
+        {"salesperson" : "Chris", "sales": 5}
+    ]
+
+    add() {
+        this.data.push({"salesperson" : "Test", "sales": 20});
+    }
+}
