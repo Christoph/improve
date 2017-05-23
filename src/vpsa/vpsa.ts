1ee1af634260ebb3d254db869bb11c3e91e2d3ec
Chris <christoph.kralj@gmail.com>
2017-05-23 12:51:18 +0200

    Used http loader and started implementing dynamic bar chart

--- src/vpsa/vpsa.ts
+++ src/vpsa/vpsa.ts
@@ -1,12 +1,23 @@
-export class Vspa {
-    data = [
-        {"salesperson" : "Bob", "sales": 12},
-        {"salesperson" : "Robin", "sales": 1},
-        {"salesperson" : "Anne", "sales": 8},
-        {"salesperson" : "Chris", "sales": 5}
-    ]
+import {inject} from 'aurelia-framework';
+import {LocalLoader} from '../resources/sources/local-loader';
 
-    add() {
-        this.data.push({"salesperson" : "Test", "sales": 20});
+@inject(LocalLoader)
+export class Vspa {
+    param1 = 1;
+    param2 = 1;
+    private loader;
+
+    constructor(loader) {
+        this.loader = loader;
+
+        this.model();
+    }
+
+    model() {
+        this.loader.load_file("regio.json");
+    }
+
+    out() {
+        console.log(this.loader.data)
     }
 }
