140ae3a599f5e37bf146122f1579b19db1547f96
Christoph <christoph.kralj@gmail.com>
2017-09-22 16:02:46 +0200

    Added variable event chance to the sampling

--- src/drift/drift.ts
+++ src/drift/drift.ts
@@ -1,58 +1,31 @@
 import {autoinject, observable} from 'aurelia-framework';
 import "ion-rangeslider"
-import {SIR} from "../models/sir"
+import {Genetic} from "../models/genetic"
 import * as d3 from "d3"
 
 @autoinject
 export class Drift {
+    @observable brushing_p;
     @observable brushing_pop;
-    @observable brushing_sus;
-    @observable brushing_inf;
-    @observable brushing_rec;
+    redraw_p;
     redraw_pop;
-    redraw_sus;
-    redraw_inf;
-    redraw_rec;
 
     @observable brushing_parallel;
     redraw_parallel;
 
     inFilter = []
 
-    // SIR Model
-    TS = 1.0
-    NY = 1
-    ND = this.NY*365
+    // Genetic Model
+    simulations = 100;
+    generations = 100;
 
-    S0 = 0.1
-    I0 = 1e-4
-    R0 = 1 - this.S0 - this.I0
+    // Population count
+    pop_from = 0;
+    pop_to = 1000;
 
-    // Mortality probabilty, I dies before natural death or recovery
-    rho = 0.5
-    rho_n = 5;
-    rho_from = 40;
-    rho_to = 60;
-    // Per captia death rate  from natural causes
-    v = 1/(70*365.0)
-    v_n = 5;
-    v_from = 70
-    v_to = 80
-    // Population birth rate
-    mu = 1/(70*365.0);
-    mu_n = 5;
-    mu_from = 70;
-    mu_to = 180;
-    // Transmission rate: I -> S. Includes encounter and transmission rate
-    beta = 520/365.0
-    beta_n = 5;
-    beta_from = 0.9
-    beta_to = 1.1
-    // Recovery rate
-    gamma=1/7.0
-    gamma_n = 5;
-    gamma_from = 4;
-    gamma_to = 6;
+    // Probability of event
+    event_from = 0.1;
+    event_to = 0.2;
 
     // Sobol sampling
     sobol_n = 100
@@ -66,78 +39,37 @@ export class Drift {
 
     attached() {
         // Attaching range sliders
-        $("#rho").ionRangeSlider({
-            min: 0,
-            max: 100,
-            from: 40,
-            to: 60,
-            step: 1,
-            type: "double",
-            postfix: "%",
-            grid: true,
-            grid_num: 10,
-            onFinish: (data) => {
-            this.rho_from = data["from_pretty"]
-            this.rho_to = data["to_pretty"]
-        }});
-        $("#v").ionRangeSlider({
-            min: 10,
-            max: 150,
-            from: 70,
-            to: 80,
-            step: 1,
-            type: "double",
-            prefix: "1/",
-            postfix: " per year",
-            grid: true,
-            grid_num: 10,
-            onFinish: (data) => {
-            this.v_from = data["from_pretty"]
-            this.v_to = data["to_pretty"]
-        }});
-        $("#mu").ionRangeSlider({
-            min: 10,
-            max: 150,
-            from: 70,
-            to: 80,
-            step: 1,
-            type: "double",
-            prefix: "1/",
-            postfix: " per year",
-            grid: true,
-            grid_num: 10,
-            onFinish: (data) => {
-            this.mu_from = data["from_pretty"]
-            this.mu_to = data["to_pretty"]
-        }});
-        $("#beta").ionRangeSlider({
-            min: 0,
-            max: 2,
-            from: 0.9,
-            to: 1.1,
-            step: 0.1,
-            type: "double",
-            postfix: " per day",
-            grid: true,
-            grid_num: 10,
-            onFinish: (data) => {
-            this.gamma_from = data["from_pretty"]
-            this.gamma_to = data["to_pretty"]
-        }});
-        $("#gamma").ionRangeSlider({
+        $("#pop").ionRangeSlider({
             min: 1,
-            max: 20,
-            from: 4,
-            to: 6,
+            max: 10000,
+            from: 500,
+            to: 1000,
             step: 1,
             type: "double",
-            prefix: "1/",
             grid: true,
             grid_num: 10,
             onFinish: (data) => {
-            this.gamma_from = data["from_pretty"]
-            this.gamma_to = data["to_pretty"]
+            this.pop_from = data["from"]
+            this.pop_to = data["to"]
         }});
+        $("#event").ionRangeSlider({
+            min: 0.01,
+            max: 1,
+            from: 0.1,
+            to: 0.2,
+            step: 0.01,
+            type: "double",
+            grid: true,
+            grid_num: 10,
+            onFinish: (data) => {
+            this.event_from = data["from"]
+            this.event_to = data["to"]
+        }});
+    }
+
+    brushing_pChanged() {
+        this.updateOutData(this.brushing_p);
+        this.updateInData(this.brushing_p);
     }
 
     brushing_popChanged() {
@@ -145,32 +77,9 @@ export class Drift {
         this.updateInData(this.brushing_pop);
     }
 
-    brushing_susChanged() {
-        this.updateOutData(this.brushing_sus);
-        this.updateInData(this.brushing_sus);
-    }
-
-    brushing_recChanged() {
-        this.updateOutData(this.brushing_rec);
-        this.updateInData(this.brushing_rec);
-    }
-
-    brushing_infChanged() {
-        this.updateOutData(this.brushing_inf);
-        this.updateInData(this.brushing_inf);
-    }
-
-    brushing_parallelChanged() {
-        this.updateParallelData(this.brushing_parallel);
-        this.filterOutData(this.brushing_parallel);
-        // this.redrawLinecharts();
-    }
-
     redrawLinecharts() {
+        this.redraw_p = this.redraw_p == 0 ? 1 : 0;
         this.redraw_pop = this.redraw_pop == 0 ? 1 : 0;
-        this.redraw_inf = this.redraw_inf == 0 ? 1 : 0;
-        this.redraw_rec = this.redraw_rec == 0 ? 1 : 0;
-        this.redraw_sus = this.redraw_sus == 0 ? 1 : 0;
     }
 
     private updateOutData(mapping) {
@@ -238,50 +147,39 @@ export class Drift {
         }
     }
 
-    vspa() {
+    compute() {
         // Create model params
-        let sir = new SIR(
-          [this.rho_from, this.rho_to],
-          [this.v_from, this.v_to],
-          [this.mu_from, this.mu_to],
-          [this.beta_from, this.beta_to],
-          [this.gamma_from, this.gamma_to],
-          this.NY, this.S0, this.I0)
+        let gen = new Genetic([this.event_from, this.event_to],[this.pop_from, this.pop_to], this.generations)
 
-          let model = sir.compute_model(this.sobol_n)
+        let model = gen.compute_model(this.simulations)
 
-          model.forEach( (sol, run) => {
-            let temp = <any[]> []
-            for (let i = 0; i < sol.length; i++) {
-                temp.push({
-                    "x": sir.time_range[i],
-                    "sus": sol[i][0],
-                    "inf": sol[i][1],
-                    "rec": sol[i][2],
-                    "pop": sol[i][0] + sol[i][1] + sol[i][2]
-                })
-            }
+        model.forEach( (sol, run) => {
+          let temp = <any[]> []
+          for (let i = 0; i < sol.length; i++) {
+              temp.push({
+                  "x": gen.time_range[i],
+                  "p": sol[i].p,
+                  "pop": sol[i].pop
+              })
+          }
 
-            this.data_parallel.push({
-                    "id": run,
-                    "highlight": 0,
-                    "data": {
-                        "rho": sir.params[run][0],
-                        "v": sir.params[run][1],
-                        "mu": sir.params[run][2],
-                        "beta": sir.params[run][3],
-                        "gamma": sir.params[run][4]
-                    }
-            })
-
-            this.data_lines_original.push({
-                    "id": run,
-                    "highlight": 0,
-                    "data": temp
-            })
+          this.data_parallel.push({
+                  "id": run,
+                  "highlight": 0,
+                  "data": {
+                      "pop": gen.params[run][0],
+                      "prob": gen.params[run][1]
+                  }
           })
 
-          this.data_length = this.data_lines_original.length;
-          this.filterOutData([])
-        }
+          this.data_lines_original.push({
+                  "id": run,
+                  "highlight": 0,
+                  "data": temp
+          })
+        })
+
+        this.data_length = this.data_lines_original.length;
+        this.filterOutData([])
+      }
 }
